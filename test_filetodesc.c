8453dc0dbd4e53ddd875befd15a991f8c04308b1
eKat <katerina@mcs51.ru>
2017-02-15 16:31:35 +0300

    1. In schema.yaml you can set LEVEL(and other args) using >=, <=, >, <. 2. User can set LEVEL (and other args) using strings (="error") or digits (=0). 3. test_filetodesc.c (and a.out) updated: now you should send files in the following order: disp info warn dbglog dbg syswarn syserr sysdbg

--- test_filetodesc.c
+++ test_filetodesc.c
@@ -1,7 +1,8 @@
 /******************************************************************************
  * 																			  *
- * Открывает N файлов и отправляет их содержимое в N файловых дескрипторов.   *
- * Пути к файлам — аргументы к программе.									  *
+ *  Открывает N файлов и отправляет их содержимое в N файловых дескрипторов.  *
+ *  Пути к файлам — аргументы к программе.
+ *  disp info warn dbglog dbg syswarn syserr sysdbg                           *
  * 																			  *
  * ***************************************************************************/
  
@@ -16,8 +17,10 @@ int main (int argc, char **argv)
 	FILE * files[8];
 	FILE * streams[8];
     extern char **environ;
+    char* order[] = {"disp", "info", "warn", "dbglog", "dbg", "syswarn", "syserr", "sysdbg"};    
     
     int descs[10];
+    char th_name[8][16];
     size_t sz;
 	char buf[128];
     int i=0,j=0, k=0;
@@ -28,17 +31,22 @@ int main (int argc, char **argv)
         {
            for(k=0;environ[i][k]!='=';k++);
            descs[j]=atoi(environ[i]+k+1);
+           strncpy(th_name[j], environ[i]+9,k-9);
+           th_name[j][k-9]='\0';
            j++;
         }
         i++;
     }    
-
+    
 	for (i=0; i<argc-1;i++)
 	{
 		files[i]=fopen(argv[i+1], "r");
 		if (files[i] <= 0) printf("File %s doesn't exist.\n", argv[i+1]), exit(1);
-		streams[i]=fdopen(descs[i], "w");
-		if(streams[i]==0) printf("Can't open desc %d.\n", descs[i]), exit(2);
+        
+        for (j=0;j<8;j++) if( strcmp(order[i], th_name[j]) == 0 ) break;
+      
+		streams[i]=fdopen(descs[j], "w");
+		if(streams[i]==0) printf("Can't open desc %d.\n", descs[i], i), exit(2);
 		
 		do
 		{
